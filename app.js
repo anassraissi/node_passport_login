cbfa19cee6fff43b17d26a1905e2a3661e4eaa11
ANASS RAISSI <88003906+anassraissi@users.noreply.github.com>
2023-06-12 21:07:47 +0100

    validation register controle by err message  and create user model for database

--- app.js
+++ app.js
@@ -15,9 +15,18 @@ mongoose.connect(db,{useNewUrlParser:true})
 //EJS
 app.use(expressLayouts);
 app.set('view engine','ejs');
+//bodyParser
+
+app.use(express.urlencoded({extended:false}));
+/**
+ * The express.urlencoded() function is a built-in middleware function in Express.
+ *  It parses incoming requests with URL-encoded payloads and is based on a body parser.
+    If extended is false, you can not post "nested object"
+ */
+
 
 //routes
 app.use('/',require('./routes/index'))
-app.use('/users',require('./routes/users'))
+app.use('/users',require('./routes/users')) // import all route that have user.js file
 
 app.listen(PORT,console.log(`Server started on port ${PORT}`));
--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,40 @@ const router=express.Router();
 router.get('/login',(req,res)=> res.render('login'))
 //register page
 router.get('/register',(req,res)=> res.render('register'))
+//post register
+
+router.post('/register',(req,res)=>{
+    
+    const {name,email,password,password2}=req.body
+    let err=[];
+
+    //check required fields
+    if (!name ||!email || !password || !password2 ) {
+        err.push({msg:'Please fill all the fields'})
+    } 
+    //check password match
+    if (password!=password2){
+        err.push({msg: 'passwords do not match '}) 
+    }
+    
+    //check pass length
+    if (password.length<6){
+        err.push({msg: 'Please should be at least 6 character' }) 
+    } 
+    
+    if(err.length>0){
+        res.render('register',{
+            err,
+            name,
+            email,
+            password,
+            password2
+        })
+        console.log(err)
+    }
+    else{
+        res.send('pass')
+    }
+})
 
 module.exports=router;
