19d94f9b953a4677fed0699e8e1438004a59a723
ANASS RAISSI <88003906+anassraissi@users.noreply.github.com>
2023-06-13 22:18:13 +0100

    add user from register with hash and salting password

--- README.md
+++ README.md
@@ -6,3 +6,16 @@ npm i express bcryptjs passport passport-local ejs express-ejs-layouts mongoose 
  npm i -D nodemon
  
 
+# salting and hashing 
+
+> blockquote Hashing takes plaintext data elements and converts them into consistent ciphertext outputs used for data verification. Salting adds random characters to data, like passwords, to thwart hackers who look for consistent words and phrases in sensitive data in order to decode it.23 jui. 2022
+
+
+```
+bcrypt.genSalt(saltRounds, function(err, salt) {
+  bcrypt.hash(password, salt, function(err, hash) {
+  // returns hash
+  console.log(hash);
+  });
+});
+```
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express =require('express')
 const router=express.Router();
+const User=require('../model/User');
+const bcrypt=require('bcryptjs');
 
 // login page
 router.get('/login',(req,res)=> res.render('login'))
@@ -8,7 +10,7 @@ router.get('/register',(req,res)=> res.render('register'))
 //post register
 
 router.post('/register',(req,res)=>{
-    
+
     const {name,email,password,password2}=req.body
     let err=[];
 
@@ -16,14 +18,15 @@ router.post('/register',(req,res)=>{
     if (!name ||!email || !password || !password2 ) {
         err.push({msg:'Please fill all the fields'})
     } 
+    
     //check password match
     if (password!=password2){
         err.push({msg: 'passwords do not match '}) 
     }
     
     //check pass length
-    if (password.length<6){
-        err.push({msg: 'Please should be at least 6 character' }) 
+    if (password && password.length<6){
+        err.push({msg: ' Please password should be at least 6 character' }) 
     } 
     
     if(err.length>0){
@@ -37,7 +40,45 @@ router.post('/register',(req,res)=>{
         console.log(err)
     }
     else{
-        res.send('pass')
+
+           //Validation passed
+           User.findOne({email:email})
+           .then(user=>{
+            if(user){
+                err.push({msg: 'Email is already registred'});
+                res.render('register',{
+                    err,
+                    name,
+                    email,
+                    password,
+                    password2
+                });
+            }
+            else{
+                const newUser=new User({
+                    name,
+                    email,
+                    password,
+                });
+                //hashing password
+                bcrypt.genSalt(10, function(err, salt) {
+                    bcrypt.hash(newUser.password, salt, function(err, hash) {
+                    if(err) throw err
+                    //set password to hash
+                    newUser.password=hash
+                    //save user
+                    newUser.save()
+                    .then(user=>{
+                        res.redirect('/user/login')
+                    })
+                    .catch(err=>{console.log(err) })
+                    });
+
+            })
+            
+        } 
+                
+    })
     }
 })
 
